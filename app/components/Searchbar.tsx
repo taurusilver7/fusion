f81c7585c9fc49e98fb5ac88e258c329df598da1
Devi prasad <prasad20697@gmail.com>
2023-09-09 18:58:49 +0530

    search bar

---
+++ app/components/Searchbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Searchbar = () => {
+  return (
+    <div className='flex justify-center items-center p-3 w-full bg-black/40'>
+
+      <div className="w-4/6" />
+
+    </div>
+  )
+}
+
+export default Searchbar
