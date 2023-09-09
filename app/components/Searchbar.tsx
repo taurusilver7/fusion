26967d8f678ea5ae642f3e77d2214c03e8ec6182
Devi prasad <prasad20697@gmail.com>
2023-09-09 19:08:21 +0530

    searchbar logo

--- app/components/Searchbar.tsx
+++ app/components/Searchbar.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Searchbar = () => {
-  return (
-    <div className='flex justify-center items-center p-3 w-full bg-black/40'>
+	return (
+		<div className="flex justify-center items-center p-3 w-full bg-black/40">
+			<div className="w-1/6 flex justify-center items-center text-neutral-100">
+				<a href={"/"} className="flex justify-center items-center">
+               <img src="/logo.png" alt="logo" className="w-10 object-cover rounded-full" />
+            </a>
+			</div>
 
-      <div className="w-4/6" />
+			<div className="w-4/6" />
+			<div className="w-4/6" />
+		</div>
+	);
+};
 
-    </div>
-  )
-}
-
-export default Searchbar
+export default Searchbar;
