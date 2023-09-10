58ccdf408f96fea89d8fc328f674d153191753d1
Devi prasad <prasad20697@gmail.com>
2023-09-10 11:07:09 +0530

    api response types

---
+++ types.d.ts
@@ -0,0 +1,43 @@
+type Standing = {
+	league: League;
+};
+
+type League = {
+	id: number;
+	name: string;
+	country: string;
+	logo: string;
+	flag: string;
+	season: number;
+	standings: [Team[]];
+};
+
+type Team = {
+	rank: number;
+	team: {
+		id: number;
+		name: string;
+		logo: string;
+	};
+	points: number;
+	goalsDiff: number;
+	group: string;
+	form: string;
+	status: string;
+	description: string;
+	all: Games;
+	home: Games;
+	away: Games;
+};
+
+type Games = {
+	played: number;
+	win: number;
+	draw: number;
+	lose: number;
+	goals: {
+		for: number;
+		against: number;
+	};
+};
+export {Standing}
