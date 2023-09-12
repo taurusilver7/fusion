9decd9a88b8d279bb0529845728bbcc7a2c8aa0a
Devi prasad <prasad20697@gmail.com>
2023-09-12 09:42:51 +0530

    image domains whitelisted

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	reactStrictMode: true,
+	iamges: {
+		domains: [
+			"media-1.api-sports.io",
+			"media-2.api-sports.io",
+			"media-3.api-sports.io",
+		],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
