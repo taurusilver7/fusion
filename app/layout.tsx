2f668d10800a01a1490dd06b980105267ea42441
Devi prasad <prasad20697@gmail.com>
2023-09-12 09:43:50 +0530

    children node checklisted

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Searchbar from "./components/Searchbar";
+import Searchbar from "./components/searchBar/Searchbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
