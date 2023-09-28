0566d089ba1e275c1767f89a85247e87ee6b5890
Devi prasad <prasad20697@gmail.com>
2023-09-28 22:17:51 +0530

    getstandings

--- utils/getStandings.tsx
+++ utils/getStandings.tsx
@@ -5,9 +5,8 @@ import { USE_SAMPLE } from "../sampleData/useSample";
 import getStandingsSample from "../sampleData/getStandingsSample";
 
 async function getStandings(): Promise<Standing[]> {
-	if (USE_SAMPLE) {
-		return getStandingsSample();
-	}
+	if (USE_SAMPLE) return getStandingsSample();
+
 	const currentTime = moment();
 	const month = currentTime.month();
 	let year;
