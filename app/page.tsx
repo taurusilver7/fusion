280aa3e833e60f410624c680999c5aa321aab8d7
Devi prasad <prasad20697@gmail.com>
2023-09-28 22:17:06 +0530

    main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
+import StandingsAndFixtures from "@/components/home/StandingsAndFixtures";
+import { Standing } from "@/types";
+import getStandings from "@/utils/getStandings";
 
-export default function Home() {
-	return <div>Main body</div>;
+export default async function Home() {
+	const standingsData: Standing[] = await getStandings();
+	return (
+		<div className="flex flex-col justify-center items-center w-full md:p-10">
+			<StandingsAndFixtures standingsData={standingsData} />
+		</div>
+	);
 }
