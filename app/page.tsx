9fcafe3abf37065fb025b61637ad8d4403e9bf10
Devi prasad <prasad20697@gmail.com>
2023-09-29 13:24:36 +0530

    home page getStandings condition added

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,14 @@ import StandingsAndFixtures from "@/components/home/StandingsAndFixtures";
 import { Standing } from "@/types";
 import getStandings from "@/utils/getStandings";
 
+export const revalidate = 60;
+
 export default async function Home() {
 	const standingsData: Standing[] = await getStandings();
+
+	// if (!standingsData?.length) {
+	// 	return null;
+	// }
 	return (
 		<div className="flex flex-col justify-center items-center w-full md:p-10">
 			<StandingsAndFixtures standingsData={standingsData} />
