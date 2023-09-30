d8557cd1798346a24a7cbd1f740816ebd69179bd
Devi prasad <prasad20697@gmail.com>
2023-09-30 22:52:16 +0530

    fixtures added to standings&fixtures component

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,23 @@
 import StandingsAndFixtures from "@/components/home/StandingsAndFixtures";
-import { Standing } from "@/types";
+import { AllFixtures, Standing } from "@/types";
+import getFixturesForFiveLeagues from "@/utils/getFixturesForFiveLeagues";
 import getStandings from "@/utils/getStandings";
 
 export const revalidate = 60;
 
 export default async function Home() {
 	const standingsData: Standing[] = await getStandings();
+	const filteredFixtures: AllFixtures[] = await getFixturesForFiveLeagues();
 
 	// if (!standingsData?.length) {
 	// 	return null;
 	// }
 	return (
 		<div className="flex flex-col justify-center items-center w-full md:p-10">
-			<StandingsAndFixtures standingsData={standingsData} />
+			<StandingsAndFixtures
+				standingsData={standingsData}
+				filteredFixtures={filteredFixtures}
+			/>
 		</div>
 	);
 }
