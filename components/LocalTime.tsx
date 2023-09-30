b332a8466cdffca7226e8224d5dc8696af600ef4
Devi prasad <prasad20697@gmail.com>
2023-09-30 23:20:36 +0530

    local time component

---
+++ components/LocalTime.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Fixture } from "@/types";
+import moment from "moment";
+import { useEffect, useState } from "react";
+
+type PageProps = {
+	fixture: Fixture;
+};
+const LocalTime = ({ fixture }: PageProps) => {
+	const [formattedTime, setFormattedTime] = useState("");
+
+	useEffect(() => {
+		function formatToLocalTime(timeUTC: string): string {
+			const newTime = moment(timeUTC);
+			const localDateString = newTime.format("dddd, LL");
+			const localTimeString = newTime.format("LT");
+			return `${localDateString} ${localTimeString}`;
+		}
+
+		const fixtureTime = fixture.fixture.date;
+		const formatted = formatToLocalTime(fixtureTime);
+		setFormattedTime(formatted);
+	}, []);
+
+	return <div>{formattedTime}</div>;
+};
+
+export default LocalTime;
