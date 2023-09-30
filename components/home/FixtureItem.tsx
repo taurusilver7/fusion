d5ea088406ccbb8e8b66b306179f3548d346cdbd
Devi prasad <prasad20697@gmail.com>
2023-09-30 23:12:18 +0530

    fixture induvidual item

--- components/home/FixtureItem.tsx
+++ components/home/FixtureItem.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Fixture } from "@/types";
 import moment from "moment";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -13,8 +14,25 @@ const FixtureItem = ({ match, index }: PageProps) => {
 	const today = moment();
 	const matchDate = moment(match.fixture.date);
 
-
-	return today.isBefore(matchDate) ? (<Link href={`/match/${match.fixture.id}`} key={match.fixture.id} className={`flex w-full p-2 justify-center items-center h-36 hover:bg-red-800/50 ${index % 2 === 0 ? "bg-black/40": ""} animated-div`}>FixtureItem component</Link>) : null;
+	return today.isBefore(matchDate) ? (
+		<Link
+			href={`/match/${match.fixture.id}`}
+			key={match.fixture.id}
+			className={`flex w-full p-2 justify-center items-center h-36 hover:bg-red-800/50 ${
+				index % 2 === 0 ? "bg-black/40" : ""
+			} animated-div`}
+		>
+			<div className="w-1/3 flex flex-col justify-center items-center text-center">
+				<Image
+					src={match.teams.home.logo}
+					alt="homeLogo"
+					width={70}
+					height={70}
+				/>
+			</div>
+			FixtureItem component
+		</Link>
+	) : null;
 };
 
 export default FixtureItem;
